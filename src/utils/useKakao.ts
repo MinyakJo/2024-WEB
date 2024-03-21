040e84ad8f9e3523fa505768357ee303831fd2a4
MinyakJo <cmh8037@gmail.com>
2024-03-21 15:39:13 +0900

    카카오 api 변경으로 인한 수정

--- src/utils/useKakao.ts
+++ src/utils/useKakao.ts
@@ -3,8 +3,9 @@ import { useCookies } from "react-cookie";
 import { useLocation, useNavigate } from "react-router-dom";
 import QueryString from "qs";
 import { fetch } from "apis/fetch";
-// import { useSetRecoilState } from "recoil";
-// import { loginHiddenAlertState } from "recoil/loginAtom";
+import { getAccessTokenByKakao } from "apis/kakao";
+import { useSetRecoilState } from "recoil";
+import { loginHiddenAlertState } from "recoil/loginAtom";
 
 const useKakao = () => {
   //useLocation
@@ -14,23 +15,43 @@ const useKakao = () => {
   const navigate = useNavigate();
 
   //cookie
-  const [, setCookie] = useCookies(["kakao_code", "loginId", "id", "token"]);
+  const [, setCookie] = useCookies(["kakao_token", "loginId", "id", "token"]);
 
   //recoil
-  //   const setHiddenAlert = useSetRecoilState(loginHiddenAlertState);
+  const setHiddenAlert = useSetRecoilState(loginHiddenAlertState);
 
   //function
   const setTokenAndGetInfo = async (code: string) => {
-    const fetchData = await fetch({
-      method: "GET",
-      url: `/auth/kakao/sign-in-by-code?code=${code}`,
-    });
-    if (fetchData.data?.statusCode === 201) {
-      setCookie("id", fetchData.data.result.id);
-      setCookie("token", `Bearer ${fetchData.data.result.jwt}`);
-      navigate("/", { replace: true });
+    const getToken = await getAccessTokenByKakao(code);
+
+    if (getToken !== undefined) {
+      setCookie("kakao_token", getToken);
+
+      const fetchData = await fetch({
+        method: "POST",
+        url: `/auth/kakao/sign-in-by-token`,
+        data: {
+          accessToken: getToken,
+        },
+      });
+
+      if (
+        fetchData.data?.statusCode === 201 ||
+        fetchData.data?.statusCode === 200
+      ) {
+        setCookie("loginId", fetchData.data.result?.loginId);
+        setCookie("id", fetchData.data.result.id);
+        setCookie("token", `Bearer ${fetchData.data.result.jwt}`);
+        navigate("/", { replace: true });
+      } else {
+        alert("카카오 계정이 없어 회원가입 페이지로 이동");
+        navigate("/sign-up", { replace: true });
+      }
     } else {
-      navigate("/sign-up", { replace: true });
+      setHiddenAlert({
+        text: "코드 받아오기 실패",
+        isHidden: false,
+      });
     }
   };
 
@@ -40,7 +61,6 @@ const useKakao = () => {
       //쿼리 스트링을 읽어와서 recoil 등록
       const id = QueryString.parse(search, { ignoreQueryPrefix: true });
       if (typeof id?.code === "string") {
-        setCookie("kakao_code", id.code);
         setTokenAndGetInfo(id.code);
       }
     }
